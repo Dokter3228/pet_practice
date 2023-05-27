9c9b4f8704da026f2765475ead3ce4e613b0f4e8
Dokter3228 <Dokter322@yandex.ru>
2023-05-27 11:14:47 +0300

    init

---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export const func = (num: number) => {
+    console.log("Ура")
+    console.log(num)
+}
---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require("webpack")
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+};
