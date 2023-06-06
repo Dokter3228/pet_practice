d53922e3b62fa9190fb9eeceec4059c88ba19b38
dokter <Dokter322@yandex.ru>
2023-06-06 23:34:33 +0300

    fix infinite analyzer loop after build in prod build

--- config/builders/buildPlugins.ts
+++ config/builders/buildPlugins.ts
@@ -6,14 +6,20 @@ import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [new HtmlWebpackPlugin({ template: paths.html }),
+  const plugins = [new HtmlWebpackPlugin({ template: paths.html }),
     new webpack.ProgressPlugin(), new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
     }), new webpack.DefinePlugin({
       _IS_DEV_: JSON.stringify(isDev)
-    }), new webpack.HotModuleReplacementPlugin(),
+    }),
     new ReactRefreshPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false })
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+
+  return plugins;
 }
